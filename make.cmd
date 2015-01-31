3a796ad198673c17ab551ba5ceb2d3da4a14936c
Oliver Zieba <o.zieba@curasystems.de>
2015-01-31 18:47:35 +0100

    allow make script downloading missing nuget packages and copy dll files

--- make.cmd
+++ make.cmd
@@ -4,7 +4,6 @@ msbuild dcmedit.sln /p:Configuration=Debug /p:Platform="Any CPU"
 mkdir Deploy
 xcopy bin\Debug\dcmedit.exe Deploy
 
-rem todo copy dlls and xmls from package dirs
-xcopy /S packages\* Deploy
-xcopy dcmtk\* Deploy\dcmtk
+for /R packages %%f in (*.dll) do copy %%f Deploy
+xcopy dcmtk\* Deploy\dcmtk\
 xcopy img\* Deploy
